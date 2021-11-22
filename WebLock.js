8a8f9b54556627927177096f6f23133370e66c1e
holyget <76859564+holyget@users.noreply.github.com>
2021-11-22 23:49:14 +0800

    Add files via upload

---
+++ WebLock.js
@@ -0,0 +1,18 @@
+<script language="JavaScript">
+while (true) {
+    var password = "";
+    password = prompt('请输入口令:', '');
+    if (password != '1') {
+        alert("口令错误");
+        
+   window.opener = null;
+   window.open("about:blank", "_self");
+   window.close();        
+   break;
+
+    } else {
+
+        break;
+    }
+}
+			</script>
