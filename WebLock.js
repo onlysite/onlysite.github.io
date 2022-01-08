244265b869a3d4804bc3f2f30cf7f2b274e5026e
holyget <76859564+holyget@users.noreply.github.com>
2022-01-08 22:48:24 +0800

    Update WebLock.js

--- WebLock.js
+++ WebLock.js
@@ -1,18 +1,11 @@
 <script language="JavaScript">
-while (true) {
-    var password = "";
-    password = prompt('请输入口令:', '');
-    if (password != '1') {
-        alert("口令错误");
-        
-   window.opener = null;
-   window.open("about:blank", "_self");
-   window.close();        
-   break;
-
-    } else {
-
-        break;
-    }
-}
-			</script>
+			while(true) {
+				var password = "";
+				password = prompt('请输入密码:', '');
+				if(password != '1') {
+					alert("出现错误!!");
+				} else {
+					break;
+				}
+			}
+		</script>
